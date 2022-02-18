43e5b238c029ffbbe8ffdc19c9cebe345fb37412
Sword Saint <toram5435@gmail.com>
2022-02-18 16:57:52 +0800

    checking

---
+++ redirector/index.php
@@ -0,0 +1,99 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>IRCS | REDIRECTOR</title>
+  <link rel="icon" href="dist/img/ircs.png" type="image/x-icon" />
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- uPlot -->
+  <link rel="stylesheet" href="plugins/uplot/uPlot.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+</head>
+<body class="hold-transition sidebar-mini">
+  <br>
+  <br>
+  <br>
+
+<div class="wrapper">
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+               <div class="row">
+          <div class="col-12">
+              <h1 style="text-align: center;"><b>IRCS DATA Search</b></h1>
+          </div>
+    
+        </div>
+
+         <div class="container-fluid">
+
+          <div class="row">
+              <div class="col-4">
+                 <div class="card card-primary">
+          <div class="card-header">
+            <h3 class="card-title"><h5 style="text-align:center;">Data Search (1 month)</h3> </h5>              
+              </div>
+               <div class="card-body">
+                  <div style="justify-content: center; display: flex;">
+                  <a href="http://172.25.114.90/data_search/latest/" class="btn btn-primary float-right" target="blank">Proceed  <i class="fas fa-arrow-right"></i></a></div>
+                </div>
+            </div>
+              </div>
+              <div class="col-4">
+                    <div class="card card-info">
+          <div class="card-header">
+            <h3 class="card-title"><h5 style="text-align:center;">Data Search (3 months)</h3></h5>
+              </div>
+              <div class="card-body" style="justify-content: center; display: flex;">
+                  <a href="http://172.25.114.90/data_search/data_search/" class="btn btn-info" target="blank">Proceed  <i class="fas fa-arrow-right"></i></a>
+                </div>
+            </div>
+              </div>
+              <div class="col-4">
+                    <div class="card card-secondary">
+          <div class="card-header">
+            <h3 class="card-title"><h5 style="text-align:center;">Data Search (6 months)</h3></h5>
+              </div>
+              <div class="card-body" style="justify-content: center; display: flex;">
+                  <a href="http://172.25.114.90/data_search/my_data_search/index.php" class="btn btn-secondary " target="blank">Proceed  <i class="fas fa-arrow-right"></i></a>
+                </div>
+            </div>
+              </div>
+
+          </div>
+          
+  
+
+        <!-- /.card -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+ 
+
+
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- uPlot -->
+<script src="plugins/uplot/uPlot.iife.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="dist/js/demo.js"></script>
+<!-- Page specific script -->
+
+</script>
+</body>
+</html>
